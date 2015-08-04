5fcb88b06209b48e8b85b12f5a56c0935de6ad97
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 01:53:42 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/google-maps-bank/tags/1.0.30@1212213 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/get-map-settings.php
@@ -0,0 +1,294 @@
+<?php
+if (!is_user_logged_in())
+{
+	return;
+}
+else
+{
+	switch($gmb_role)
+	{
+		case "administrator":
+			$user_role_permission = "manage_options";
+		break;
+		case "editor":
+			$user_role_permission = "publish_pages";
+		break;
+		case "author":
+			$user_role_permission = "publish_posts";
+		break;
+	}
+	if (!current_user_can($user_role_permission))
+	{
+		return;
+	}
+	else
+	{
+		if(isset($_REQUEST["map_id"]))
+		{
+			$map_id = intval($_REQUEST["map_id"]);
+		}
+		$map_settings = $wpdb->get_results
+		(
+			$wpdb->prepare
+			(
+				"SELECT * FROM " . map_bank_meta_table(). " INNER JOIN ".map_bank_create_new_map_table().
+				" ON ".map_bank_create_new_map_table().".id = ".map_bank_meta_table().".map_id WHERE ".map_bank_create_new_map_table().
+				".id = %d or ".map_bank_create_new_map_table().".parent_id = %d ORDER BY " .map_bank_create_new_map_table().".id DESC " ,
+				$map_id,
+				$map_id
+			)
+		);
+		
+		if(!function_exists("get_map_settings"))
+		{
+			function get_map_settings($id,$map_settings,$map_type)
+			{
+				$map_settings_array = array();
+				foreach ($map_settings as $row)
+				{
+					if ($row->map_id == $id)
+					{
+						$map_settings_array["$row->map_meta_key"] = $row->map_meta_value;
+						$map_settings_array["creation_date"] = $row->creation_date;
+						if($map_type != "map")
+						{
+							$map_settings_array["id"] = $row->id;
+						}
+					}
+				}
+				return $map_settings_array;
+			}
+		}
+		
+		if(!function_exists("get_geo_settings"))
+		{
+			function get_geo_settings($id,$map_settings,$match)
+			{
+				$ids = array();
+				foreach ($map_settings as $row)
+				{
+					if ($row->parent_id == $id && $row->map_type == $match)
+					{
+						if(!in_array($row->id,$ids))
+						{
+							array_push($ids, $row->id);
+						}
+					}
+				}
+				
+				$map_marker_array = array();
+				foreach ($ids as $row)
+				{
+					$map_marker = get_map_settings($row,$map_settings,$match);
+					array_push($map_marker_array, $map_marker);
+				}
+				return array_unique($map_marker_array, SORT_REGULAR);
+			}
+		}
+		$map_settings_data = get_map_settings($map_id,$map_settings,"map");
+		if(isset($_REQUEST["page"]))
+		{
+			$marker = array();
+			if(isset($_REQUEST["map_id"]))
+			{
+				$marker = get_geo_settings($map_id,$map_settings,"marker");
+				$polygon = get_geo_settings($map_id,$map_settings,"polygon");
+				$polyline = get_geo_settings($map_id,$map_settings,"polyline");
+				$map_type_data= isset($map_settings_data["map_type"]) ? $map_settings_data["map_type"] : "1";
+				$map_themes = isset($map_settings_data["map_themes"]) ? $map_settings_data["map_themes"] : "default1";
+				$map_language = isset($map_settings_data["map_language"]) ? $map_settings_data["map_language"] : "en";
+				$map_location_longitude = isset($map_settings_data["longitude"]) ? $map_settings_data["longitude"] : "-97.03259696914063";
+				$map_location_latitude = isset($map_settings_data["latitude"]) ? $map_settings_data["latitude"] : "35.38453628611739";
+					
+				$map_kml_layer_update = "0";
+				$map_kml_layer_link_update = "";
+				$map_weather_layer_update =  "";
+				$map_temprature_unit_update = "0";
+				$map_wind_speed_unit_update =  "0";
+				$map_fussion_table_layer_update = "";
+				$map_fussion_layer_key_update =  "Geocodable address";
+				$map_fussion_layer_table_name_update = "1mZ53Z70NsChnBMm-qEYmSDOvLXgrreLTkQUvvg";
+				$map_traffic_layer_update =  "";
+				$map_transit_layer_update = "";
+				$map_bicycling_layer_update = "";
+				$map_panoramio_layer_update =  "";
+				$map_engine_layer_update =  "0";
+				$map_layer_id_update = "10446176163891957399-12677872887550376890";
+				
+				$map_nearest_location_update = isset($map_settings_data["nearest_location"]) ? $map_settings_data["nearest_location"] : "1";
+				$map_map_dragable_update = isset($map_settings_data["map_dragable"]) ? $map_settings_data["map_dragable"] : "1";
+				$map_pan_control_update = isset($map_settings_data["pan_control"]) ? $map_settings_data["pan_control"] : "1";
+				$map_map_type_control_update = isset($map_settings_data["map_type_control"]) ? $map_settings_data["map_type_control"] : "1";
+				$map_scale_control_update = isset($map_settings_data["scale_control"]) ? $map_settings_data["scale_control"] : "1";
+				$map_overview_control_update = isset($map_settings_data["overview_control"]) ? $map_settings_data["overview_control"] : "1";
+				$map_direction_update = "0";
+				$map_direction_text_update ="0";
+				$map_travel_mode_update = "DRIVING";
+				$map_text_color_update = "#000000";
+				$map_background_update ="#ffffff";
+				$map_border_color_update = "#000000";
+				$map_font_family_update = "inherit";
+				
+				$map_street_view_update = "0";
+				$map_close_button_update = "0";
+				$map_marker_cluster_update = "0";
+				$map_cluster_grid_update = "80";
+				$map_max_zoom_level_update ="1";
+				$map_circle_overlay_update ="0";
+				$map_circle_radius_update ="10";
+				$map_stroke_color_update ="#e65765";
+				$map_circle_weight_update = "2";
+				$map_fill_color_update = "#f8c1c3";
+				$map_circle_opacity_update = "0.8";
+			}
+			if(esc_attr($_REQUEST["page"]) != "manage_map")
+			{
+				switch($map_type_data)
+				{
+					case "1":
+						$map_type = "ROADMAP";
+						break;
+					case "2":
+						$map_type = "TERRAIN";
+						break;
+					case "3":
+						$map_type = "HYBRID";
+						break;
+					case "4":
+						$map_type = "SATELLITE";
+						break;
+				}
+			}
+			switch(esc_attr($_REQUEST["page"]))
+			{
+				case "gmb_create_new_map":
+					$map_title = $map_settings_data["map_title"];
+				break;
+				case "gmb_add_location":
+					$map_location_title = isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "35311 Westech Road, Shawnee, OK 74804, USA";
+					$map_location_country = isset($map_settings_data["country"]) ? $map_settings_data["country"] : "USA";
+				break;
+				case "gmb_add_marker":
+					$map_location_title= isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "35311 Westech Road, Shawnee, OK 74804, USA";
+					$map_marker_icon_update = "";
+					$map_marker_category_update = "1";
+					$map_marker_name_update = "";
+					$map_marker_animation_update = "0";
+					$map_info_window_update = "0";
+					$map_info_window_width_update = "400";
+					$map_info_title_update = "";
+					$map_description_update = "";
+					$map_info_dragable_update = "0";
+					$map_info_add_image_update = "1";
+					$map_info_hyperlink_update = "0";
+					$map_info_window_image_url_update = plugins_url("/assets/images/album-cover.png",dirname(__FILE__));
+					$map_info_hyperlink_text_update = "";
+					$map_info_hyperlink_url_update = "";
+					
+				break;
+				case "gmb_add_polygon":
+					$map_location_title= isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "";
+					$map_location_latitude = isset($polygon_settings["polygon_latitude"]) ? $polygon_settings["polygon_latitude"] : $map_settings_data["latitude"];
+					$map_polygon_line_color_update = "#d00108";
+					$map_polygon_line_opacity_update ="1.0";
+					$map_polygon_data_update = "";
+					$map_polygon_color_update = "#ffb3b3";
+					$map_polygon_opacity_update = "0.35";
+				break;
+				case "gmb_add_polyline":
+					$map_location_title= isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "";
+					$map_location_title = isset($polyline_settings["polyline_location"]) ? $polyline_settings["polyline_location"] : $map_settings_data["location_title"];
+					$map_polyline_color_update = "#000000";
+					$map_polyline_opacity_update = "1.0";
+					$map_polyline_thickness_update = "2";
+					$map_polyline_type_update = "1";
+					$map_polyline_data_update = "";
+				break;
+				case "gmb_edit_location":
+					$map_location_title = isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "Unnamed Road, Singapore";
+					$map_location_country = isset($map_settings_data["country"]) ? $map_settings_data["country"] : "Singapore";
+				break;
+				case "gmb_edit_marker":
+					$map_location_title= isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "35311 Westech Road, Shawnee, OK 74804, USA";
+					$map_marker_icon_update = "";
+					$map_marker_category_update = "1";
+					$map_marker_name_update = "";
+					$map_marker_animation_update = "0";
+					$map_info_window_update = "0";
+					
+					if(isset($_REQUEST["mid"]))
+					{
+						$marker_id = intval($_REQUEST["mid"]);
+						$marker_settings = get_map_settings($marker_id,$map_settings,"marker");
+						$map_location_title = isset($marker_settings["marker_location"]) ? $marker_settings["marker_location"] : $map_settings_data["location_title"];
+						$map_location_latitude = isset($marker_settings["marker_latitude"]) ? $marker_settings["marker_latitude"] : $map_settings_data["latitude"];
+						$map_location_longitude = isset($marker_settings["marker_longitude"]) ? $marker_settings["marker_longitude"] : $map_settings_data["longitude"];
+						$map_marker_icon_update = isset($marker_settings["map_marker"]) ? $marker_settings["map_marker"] : "";
+						$map_marker_category_update = isset($marker_settings["marker_category"]) ? $marker_settings["marker_category"] : "1";
+						$map_marker_name_update = isset($marker_settings["marker_name"]) ? stripcslashes(htmlspecialchars_decode($marker_settings["marker_name"])) :"";
+						$map_marker_animation_update = isset($marker_settings["animation"]) ? $marker_settings["animation"] : "0";
+						$map_info_window_update = isset($marker_settings["info_window"]) ? $marker_settings["info_window"] : "0";
+						$map_info_window_width_update = isset($marker_settings["info_window_width"]) ? $marker_settings["info_window_width"] : "400";
+						$map_info_title_update = isset($marker_settings["info_title"]) ? stripcslashes(htmlspecialchars_decode($marker_settings["info_title"])) : "";
+						$map_description_update = isset($marker_settings["description"]) ? stripcslashes(htmlspecialchars_decode(html_entity_decode($marker_settings["description"]))) : "";
+						$map_info_dragable_update = isset($marker_settings["info_dragable"]) ? $marker_settings["info_dragable"] : "0";
+						$map_info_add_image_update = isset($marker_settings["info_add_image"]) ? $marker_settings["info_add_image"] : "1";
+						$map_info_hyperlink_update = isset($marker_settings["info_hyperlink"]) ? $marker_settings["info_hyperlink"] : "0";
+						$map_info_window_image_url_update = isset($marker_settings["info_window_image_url"]) ? $marker_settings["info_window_image_url"] : "";
+						$map_info_hyperlink_text_update = isset($marker_settings["info_hyperlink_text"]) ? stripcslashes(htmlspecialchars_decode($marker_settings["info_hyperlink_text"])) : "";
+						$map_info_hyperlink_url_update = isset($marker_settings["info_hyperlink_url"]) ? $marker_settings["info_hyperlink_url"] : "";
+					}
+				break;
+				case "gmb_edit_polygon":
+					$map_location_title= isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "";
+					$map_polygon_line_color_update = "#d00108";
+					$map_polygon_line_opacity_update ="1.0";
+					$map_polygon_data_update = "";
+					$map_polygon_color_update = "#ffb3b3";
+					$map_polygon_opacity_update = "0.35";
+					if(isset($_REQUEST["pgon_id"]))
+					{
+						$pgon_id = intval($_REQUEST["pgon_id"]);
+						$polygon_settings = get_map_settings($pgon_id,$map_settings,"polygon");
+						$map_location_latitude = isset($polygon_settings["polygon_latitude"]) ? $polygon_settings["polygon_latitude"] : $map_settings_data["latitude"];
+						$map_location_longitude = isset($polygon_settings["polygon_longitute"]) ? $polygon_settings["polygon_longitute"] : $map_settings_data["longitude"];
+						$map_location_title = isset($polygon_settings["polygon_location"]) ? $polygon_settings["polygon_location"] : $map_settings_data["location_title"];
+						$map_polygon_line_color_update = isset($polygon_settings["polygon_line_color"]) ? $polygon_settings["polygon_line_color"] : "#d00108";
+						$map_polygon_line_opacity_update = isset($polygon_settings["polygon_line_opacity"]) ? $polygon_settings["polygon_line_opacity"] : "1.0";
+						$map_polygon_data_update = isset($polygon_settings["polygon_data"]) ? $polygon_settings["polygon_data"] : "";
+						$map_polygon_color_update = isset($polygon_settings["polygon_color"]) ? $polygon_settings["polygon_color"] : "#ffb3b3";
+						$map_polygon_opacity_update = isset($polygon_settings["polygon_opacity"]) ? $polygon_settings["polygon_opacity"] : "0.35";
+					}
+				break;
+				case "gmb_edit_polyline":
+					$map_location_title= isset($map_settings_data["location_title"]) ? $map_settings_data["location_title"] : "";
+					$map_polyline_color_update = "#000000";
+					$map_polyline_opacity_update = "1.0";
+					$map_polyline_thickness_update = "2";
+					$map_polyline_type_update = "1";
+					$map_polyline_data_update = "";
+					if(isset($_REQUEST["pline_id"]))
+					{
+						$pline_id = intval($_REQUEST["pline_id"]);
+						$polyline_settings = get_map_settings($pline_id,$map_settings,"polyline");
+						$map_location_latitude = isset($polyline_settings["polyline_latitude"]) ? $polyline_settings["polyline_latitude"] : $map_settings_data["latitude"];
+						$map_location_longitude = isset($polyline_settings["polyline_longitute"]) ? $polyline_settings["polyline_longitute"] : $map_settings_data["longitude"];
+						$map_location_title = isset($polyline_settings["polyline_location"]) ? $polyline_settings["polyline_location"] : $map_settings_data["location_title"];
+						$map_polyline_color_update = isset($polyline_settings["polyline_color"]) ? $polyline_settings["polyline_color"] : "#000000";
+						$map_polyline_opacity_update = isset($polyline_settings["polyline_opacity"]) ? $polyline_settings["polyline_opacity"] : "1.0";
+						$map_polyline_thickness_update = isset($polyline_settings["polyline_thickness"]) ? $polyline_settings["polyline_thickness"] : "2";
+						$map_polyline_type_update = isset($polyline_settings["polyline_type"]) ? $polyline_settings["polyline_type"] : "1";
+						$map_polyline_data_update = isset($polyline_settings["polyline_data"]) ? $polyline_settings["polyline_data"] : "";
+					}
+				break;
+				case "manage_map":
+					$manage_location_data = get_map_settings($map_id,$map_settings,"map");
+					$manage_marker_data = get_geo_settings($map_id,$map_settings,"marker");
+					$manage_polygon_data = get_geo_settings($map_id,$map_settings,"polygon");
+					$manage_polyline_data = get_geo_settings($map_id,$map_settings,"polyline");
+				break;
+			}
+		}
+	}
+}
+?>
---
+++ views/dashboard.php
@@ -0,0 +1,253 @@
+<?php
+if (!is_user_logged_in())
+{
+	return;
+}
+else
+{
+	switch($gmb_role)
+	{
+		case "administrator":
+			$user_role_permission = "manage_options";
+		break;
+		case "editor":
+			$user_role_permission = "publish_pages";
+		break;
+		case "author":
+			$user_role_permission = "publish_posts";
+		break;;
+	}
+	if (!current_user_can($user_role_permission))
+	{
+		return;
+	}
+	else
+	{
+		if(!function_exists("get_map_count"))
+		{
+			function get_map_count($maps, $map_settings)
+			{
+				$map_data_array = array();
+				foreach ($maps as $row)
+				{
+					$map_data = get_map_details($row->id,$map_settings);
+					array_push($map_data_array, $map_data);
+				}
+				return array_unique($map_data_array, SORT_REGULAR);
+			}
+		}
+		if(!function_exists("get_map_details"))
+		{
+			function get_map_details($id,$map_settings)
+			{
+				$map_settings_array = array();
+				foreach ($map_settings as $row)
+				{
+					if ($row->id == $id)
+					{
+						$map_settings_array["$row->map_meta_key"] = $row->map_meta_value;
+						$map_settings_array["id"] = $row->id;
+						$map_settings_array["creation_date"] = $row->creation_date;
+					}
+				}
+				return $map_settings_array;
+			}
+		}
+		
+		$delete_single_map = wp_create_nonce("one_map_delete");
+		$delete_bulk_map = wp_create_nonce("multiple_map_delete");
+		$map_data_details = $wpdb->get_results
+		(
+			$wpdb->prepare
+			(
+				"SELECT * FROM " . map_bank_meta_table(). " INNER JOIN ".map_bank_create_new_map_table().
+				" ON ".map_bank_create_new_map_table().".id = ".map_bank_meta_table().".map_id WHERE ".map_bank_create_new_map_table().
+				".parent_id = %d and " . map_bank_create_new_map_table() . ".map_type=%s ORDER BY " .map_bank_create_new_map_table().".id DESC ",
+				0,
+				"map"
+			)
+		);
+		$maps = $wpdb->get_results
+		(
+			$wpdb->prepare
+			(
+				"SELECT id FROM " . map_bank_create_new_map_table(). " WHERE parent_id = %d and map_type = %s",
+				0,
+				"map"
+			)
+		);
+		$map_count = $wpdb->get_var
+		(
+			$wpdb->prepare
+			(
+				"SELECT count(id) FROM ".map_bank_create_new_map_table()." where map_type=%s",
+				"map"
+			)
+		);
+		$map_details =  get_map_count($maps, $map_data_details);
+		?>
+		<form  class="layout-form" style="max-width:1000px;">
+			<div class="fluid-layout">
+				<div class="layout-span12">
+					<div class="widget-layout">
+						<div class="widget-layout-title">
+							<h4><?php _e("Dashboard", map_bank); ?></h4>
+						</div>
+						<div class="widget-layout-body">
+							<div class="fluid-layout">
+								<div class="layout-span12">
+									<div class="layout-control-group">
+										<select  id="ux_ddl_bulk_action" name="ux_ddl_bulk_action" class="layout-span2">
+											<option value="0"><?php _e("Bulk Action", map_bank); ?></option>
+											<option value="1"><?php _e("Delete", map_bank); ?></option>
+										</select>
+										<input type="button" id="ux_btn_action" onclick="bulk_delete();" name="ux_btn_action" class="btn btn-danger" value="<?php _e("Apply", map_bank); ?>"/>
+										<?php 
+										if($map_count < 2)
+										{
+											?>
+											<a class="btn btn-danger" href="admin.php?page=gmb_create_new_map"><?php _e("Create New Map", map_bank); ?></a>
+											<?php 
+										}
+										?>
+									</div>
+									<div class="separator-doubled"></div>
+									<table class="widefat" id="data-table-dashboard" style="background-color:#fff !important;">
+										<thead>
+											<tr>
+												<th style="width: 10%;"><input style="margin-left: 0px;" type="checkbox" id="chk_selectall" name="selectall" ></th>
+												<th style="width: 20%;"><?php _e("Title", map_bank); ?></th>
+												<th style="width: 18%;"><?php _e("Type", map_bank); ?></th>
+												<th style="width: 16%;"><?php _e("Style", map_bank); ?></th>
+												<th style="width: 18%;"><?php _e("Creation Date", map_bank);?></th>
+											</tr>
+										</thead>
+										<tbody>
+											<?php
+											$flag = 0;
+											foreach($map_details as $map)
+											{
+												$alternate = $flag % 2 == 0 ? "alternate" : "";
+												$theme_used = $wpdb->get_var
+												(
+													$wpdb->prepare
+													(
+														"SELECT themes_value FROM " . map_bank_marker_themes_table(). " WHERE themes_key = %s",
+														$map["map_themes"]
+													)
+												);
+												?>
+												<tr class="<?php echo $alternate;?>">
+													<td>
+														<input type="checkbox" value="<?php echo $map["id"]; ?>" id="ux_chk_manage_map" name="ux_chk_manage_map"/>
+													</td>
+													<td>
+														<?php echo stripcslashes(htmlspecialchars_decode($map["map_title"]));?>
+														<span class="check-bottom">
+															<a style="color:#0d1ff6;" href="admin.php?page=gmb_create_new_map&map_id=<?php echo $map["id"];?>"><?php _e("Edit", map_bank); ?></a> | 
+															<a href="#" style="color:#0d1ff6;" onclick="delete_create_map(<?php echo $map["id"]; ?>);"><?php _e("Delete", map_bank); ?></a>
+														</span>
+													</td>
+													<td>
+														<?php 
+														switch($map["map_type"])
+														{
+															case "1":
+																echo "Road Map";
+															break;
+															case "2":
+																echo "Terrain";
+															break;
+															case "3":
+																echo "Hybrid";
+															break;
+															case "4":
+																echo "Satellite";
+															break;
+														}
+														?>
+													</td>
+													<td>
+														<?php echo $theme_used ;?>
+													</td>
+													<td>
+														<?php echo date("d M, Y", strtotime($map["creation_date"])); ?>
+													</td>
+												</tr>
+											<?php
+											$flag++;
+											}
+											?>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+		<script type="text/javascript">
+			var manage_map = [];
+			jQuery(document).ready(function() 
+			{
+				oTable = jQuery("#data-table-dashboard").dataTable
+				({
+					"bJQueryUI": false,
+					"bAutoWidth": true,
+					"sPaginationType": "full_numbers",
+					"sDom": "<\"datatable-header\"fl>t<\"datatable-footer\"ip>",
+					"oLanguage": {
+					"sLengthMenu": "<span>Show entries:</span> _MENU_"
+					},
+					"aoColumnDefs": [
+						{ "bSortable": false, "aTargets": [4] }
+					]
+				});
+				jQuery(".dataTables_length").css("display","none");
+				jQuery(".dataTables_filter").css("float","right");
+				jQuery(".dataTables_filter").css("margin-bottom","10px");
+				jQuery(".dataTables_filter").css("margin-top","10px");
+			});
+			
+			jQuery("#chk_selectall").click(function ()
+			{
+				var oTable = jQuery("#data-table-dashboard").dataTable();
+				var checkProp = jQuery("#chk_selectall").prop("checked");
+				jQuery("input[type=checkbox][name=ux_chk_manage_map]", oTable.fnGetNodes()).each(function () 
+				{
+					if (checkProp) 
+					{
+						jQuery(this).attr("checked", "checked");
+					}
+					else 
+					{
+						jQuery(this).removeAttr("checked");
+					}
+				});
+			});
+			
+			function bulk_delete()
+			{
+				alert("<?php _e( "This feature is only available in Premium Editions!", map_bank ); ?>");
+			}
+			function delete_create_map(map_id)
+			{
+				var confirm_delete =  confirm("<?php _e( "Are you sure, you want to delete this Map ?", map_bank ); ?>");
+				if(confirm_delete == true)
+				{
+					jQuery.post(ajaxurl, "map_id="+map_id+"&param=single_map_delete&action=add_map_library&_wpnonce=<?php echo $delete_single_map  ;?>", function(data)
+					{
+						window.location.href = "admin.php?page=gmb_dashboard";
+					});
+				}
+			}
+			function error_message_close()
+			{
+				jQuery("#top-error").remove();
+			}
+		</script>
+	<?php 
+	}
+}
+ ?>	
